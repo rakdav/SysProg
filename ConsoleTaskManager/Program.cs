77581d9992663111519d97ba12f628c51995e83e
Gogs <gogs@fake.local>
2024-03-20 10:52:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleTaskManager/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+bool start = true;
+do
+{
+    Console.WriteLine("Меню:\n1.Создать процесс\n" +
+        "2.Просмотреть информацию о процессе\n" +
+        "3.Удалить процесс\n" +
+        "4.Показать все процессы\n" +
+        "5.Выход");
+    Console.WriteLine("Выберите пункт меню:");
+    int n = int.Parse(Console.ReadLine()!);
+    switch (n)
+    {
+        case 1:
+            {
+                Console.Write("Введите путь до программы");
+                string progPath = Console.ReadLine()!;
+                Process.Start(@$"{progPath}");
+            }
+            break;
+        case 2:
+            {
+                Console.Write("Введите id процесса для удаления:");
+                int id = int.Parse(Console.ReadLine()!);
+                Process infoProcess = Process.GetProcessById(id);
+                Console.WriteLine(infoProcess.MainModule+" "+infoProcess.PagedMemorySize);
+            }
+            break;
+        case 3:
+            {
+                try
+                {
+                    Console.Write("Введите id процесса для удаления:");
+                    int id = int.Parse(Console.ReadLine()!);
+                    Process killProcess = Process.GetProcessById(id);
+                    killProcess.Kill();
+                }
+                catch (Win32Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            break;
+        case 4: {
+                foreach (Process i in Process.GetProcesses())
+                {
+                    Console.WriteLine($"{i.Id} {i.ProcessName}");
+
+                }
+            }
+            break;
+        case 5: start = false; break;
+    }
+}
+while (start);
---
+++ SisProg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+//Process process = Process.GetCurrentProcess();
+////свойства класса process
+//Console.WriteLine(process.ProcessName);
+//Console.WriteLine(process.BasePriority);
+//Console.WriteLine(process.Handle);
+//Console.WriteLine(process.Id);
+//Console.WriteLine(process.MachineName);
+//Console.WriteLine(process.MainModule);
+//Console.WriteLine(process.VirtualMemorySize64);
+//Console.WriteLine(process.PagedMemorySize64);
+//Console.WriteLine(process.StartTime);
+
+//foreach(Process i in Process.GetProcesses())
+//{
+//    Console.WriteLine($"{i.Id} {i.ProcessName}");
+//}
+//Console.Write("Введите PID процесса:");
+//int pid = int.Parse(Console.ReadLine()!);
+//Console.WriteLine(Process.GetProcessById(pid));
+//Process proc = Process.GetProcessesByName("WebViewHost")[0];
+//ProcessThreadCollection threads = proc.Threads;
+//foreach(ProcessThread i in threads)
+//{
+//    Console.WriteLine(i.Id);
+//}
+//proc.Kill();
+Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome");
+
+
+
+
