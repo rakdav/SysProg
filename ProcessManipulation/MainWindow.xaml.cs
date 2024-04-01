ed299469aad316845d68899b9f3fffb0f083c39a
Gogs <gogs@fake.local>
2024-04-01 15:05:14 +0200

    PictureLoad-1

--- ProcessManipulation/MainWindow.xaml.cs
+++ ProcessManipulation/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +20,23 @@ namespace ProcessManipulation
     /// </summary>
     public partial class MainWindow : Window
     {
+        const uint WM_SETTEXT=0x0C;
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage ( IntPtr hwnd,uint Msg,int wParam, [MarshalAs(UnmanagedType.LPStr)] string lParam);
+        List<Process> processes = new List<Process>();
+        int Counter = 0;
         public MainWindow ()
         {
             InitializeComponent();
+            LoadAvailableAssemblies();
+        }
+        private void LoadAvailableAssemblies ()
+        {
+            string? except =new FileInfo(Process.GetCurrentProcess().ProcessName).DirectoryName;
+            DirectoryInfo currentExe = new DirectoryInfo(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            FileInfo[] f = currentExe.GetFiles();
+            var list = 0;
+            
         }
     }
 }
---
+++ ThreadProject/Program.cs
@@ -0,0 +1,62 @@
+﻿//Thread currentThread = Thread.CurrentThread;
+//Console.WriteLine(currentThread.Name);
+//currentThread.Name = "CurrentThread";
+//Console.WriteLine(currentThread.Name);
+//Console.WriteLine(currentThread.IsAlive);
+//Console.WriteLine(currentThread.Priority);
+//Console.WriteLine(currentThread.ThreadState);
+//Console.WriteLine(currentThread.ManagedThreadId);
+//Console.WriteLine(currentThread.IsBackground);
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.Write(i+" ");
+//    Thread.Sleep(1000);
+//}
+//Console.WriteLine();
+
+//Создание потоков
+//Console.WriteLine("Main thread start");
+//Thread thread1 = new Thread(Print1);
+//thread1.Start();
+//Thread thread2 = new Thread(Print2);
+//thread2.Start();
+//Thread thread3 = new Thread(() => Console.WriteLine("Омаров - пивной провокатор"));
+//thread3.Start();
+//Console.WriteLine("Main thread stop");
+//void Print1() => Console.WriteLine("Hello, world");
+//void Print2() => Console.WriteLine("Hello, Masha");
+
+//Thread thread = new Thread(Print);
+//thread.Start();
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"main поток {i}");
+//    Thread.Sleep(300);
+//}
+//void Print ()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		Console.WriteLine($"2-ой поток {i}");
+//		Thread.Sleep(300);
+//	}
+//}
+
+//Потоки с параметрами
+
+Thread thread1 = new Thread(new ParameterizedThreadStart(Print));
+Thread thread2 = new Thread(Print);
+Thread thread3 = new Thread(message => Console.WriteLine(message));
+thread1.Start("Hello1");
+thread2.Start("Hello2");
+thread3.Start("Hello3");
+for (int i = 1; i <= 6; i++)
+{
+    Thread thread4 = new Thread(PrintNumber);
+    thread4.Start(i);
+}
+void Print ( object? message ) => Console.WriteLine(message);
+void PrintNumber( object? obj )
+{
+    if (obj is int n) Console.WriteLine($"{n}*{n}={n*n}");
+}
